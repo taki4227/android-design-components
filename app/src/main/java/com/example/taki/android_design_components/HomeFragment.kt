b17283b5e913b9e202007837aa4c48c873c1bceb
taki4227 <takiki.truth@gmail.com>
2020-07-09 08:02:29 +0900

    Add menu icon with badge

--- app/src/main/java/com/example/taki/android_design_components/HomeFragment.kt
+++ app/src/main/java/com/example/taki/android_design_components/HomeFragment.kt
@@ -1,9 +1,9 @@
 package com.example.taki.android_design_components
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.taki.android_design_components.databinding.FragmentHomeBinding
 
@@ -11,11 +11,33 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
+    private var showBadge = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_home, menu)
+
+        val item = menu.findItem(R.id.announcement)
+        item.actionView.setOnClickListener {
+            showBadge = showBadge.not()
+            requireActivity().invalidateOptionsMenu()
+        }
+
+        val badge = item.actionView.findViewById<ImageView>(R.id.badge)
+        badge.isVisible = showBadge
+
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }

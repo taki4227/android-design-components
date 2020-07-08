91e85e4f8eea4a66ca168e535203000da4cd21fa
taki4227 <takiki.truth@gmail.com>
2020-07-09 07:53:18 +0900

    migrate navigation component

--- app/src/main/java/com/example/taki/android_design_components/MainActivity.kt
+++ app/src/main/java/com/example/taki/android_design_components/MainActivity.kt
@@ -2,9 +2,20 @@ package com.example.taki.android_design_components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.taki.android_design_components.databinding.ActivityMainBinding
 import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val navController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
 
     private val appBarStateChangedListener = object : AppBarStateChangedListener() {
         override fun onStateChanged(appBarLayout: AppBarLayout, state: AppBarState) {
@@ -22,7 +33,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navController.setGraph(R.navigation.nav_main)
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
 //        binding.appBar.addOnOffsetChangedListener(appBarStateChangedListener)
     }
